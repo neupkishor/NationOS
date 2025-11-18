1fd26f3a2e7f2bba54756962ad006beb20f9d9aa
Kishor Neupane <heyneupkishor@gmail.com>
2025-11-18 19:12:59 +0000

    the sidebar keep that too inside the 1440px width. that should be inside

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -2,7 +2,6 @@
 import { SidebarProvider, Sidebar, SidebarInset } from '@/components/ui/sidebar';
 import { MainNav } from '@/components/nav/main-nav';
 import { Header } from '@/components/layout/header';
-import { FirebaseClientProvider } from '@/firebase';
 
 interface AppLayoutProps {
   children: React.ReactNode;
@@ -10,20 +9,16 @@ interface AppLayoutProps {
 
 export function AppLayout({ children }: AppLayoutProps) {
   return (
-    <FirebaseClientProvider>
-      <div className="mx-auto max-w-[1440px]">
-        <SidebarProvider defaultOpen>
-          <Sidebar>
-            <MainNav />
-          </Sidebar>
-          <SidebarInset className="flex flex-col">
-            <Header />
-            <main className="flex-1 overflow-y-auto">
-              {children}
-            </main>
-          </SidebarInset>
-        </SidebarProvider>
-      </div>
-    </FirebaseClientProvider>
+    <div className="mx-auto max-w-[1440px]">
+      <SidebarProvider defaultOpen>
+        <Sidebar>
+          <MainNav />
+        </Sidebar>
+        <SidebarInset className="flex flex-col">
+          <Header />
+          <main className="flex-1 overflow-y-auto">{children}</main>
+        </SidebarInset>
+      </SidebarProvider>
+    </div>
   );
 }
